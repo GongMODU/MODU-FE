38672d01b9b3f241d6f3ace7beb3d74bf038a5db
minju3212 <smj3mjs@gmail.com>
2026-04-17 00:00:23 +0900

    🔧 chore: 디자인 시스템 타이포 파일 초기 생성

---
+++ styles/typography.ts
@@ -0,0 +1,55 @@
+import { TextStyle } from "react-native";
+
+export const typography: Record<string, TextStyle> = {
+  largeTitleMedium20: {
+    fontSize: 20,
+    fontWeight: "500",
+    lineHeight: 26,
+    letterSpacing: -1.4,
+  },
+  subtitleMedium14: {
+    fontSize: 14,
+    fontWeight: "500",
+    lineHeight: 18.2,
+  },
+  footerBold12: {
+    fontSize: 12,
+    fontWeight: "700",
+    lineHeight: 15.6,
+  },
+  bodyMedium11: {
+    fontSize: 11,
+    fontWeight: "500",
+    lineHeight: 14.3,
+  },
+  bodyRegular11: {
+    fontSize: 11,
+    fontWeight: "400",
+    lineHeight: 14.3,
+  },
+  labelMedium10: {
+    fontSize: 10,
+    fontWeight: "500",
+    lineHeight: 13,
+  },
+  bodyRegular10: {
+    fontSize: 10,
+    fontWeight: "400",
+    lineHeight: 13,
+  },
+  captionMedium9: {
+    fontSize: 9,
+    fontWeight: "500",
+    lineHeight: 11.7,
+  },
+  captionRegular9: {
+    fontSize: 9,
+    fontWeight: "400",
+    lineHeight: 11.7,
+  },
+  captionMedium8: {
+    fontSize: 8,
+    fontWeight: "500",
+    lineHeight: 10.4,
+  },
+};
