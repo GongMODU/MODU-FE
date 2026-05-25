18a8e4f510eaac50c0136f8d42498c7fd4f2b4ea
minju3212 <smj3mjs@gmail.com>
2026-05-25 18:22:14 +0900

    🎨 style: 타이포그래피 fontFamily 추가

--- styles/typography.ts
+++ styles/typography.ts
@@ -2,52 +2,62 @@ import { TextStyle } from "react-native";
 
 export const typography: Record<string, TextStyle> = {
   largeTitleMedium20: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 20,
     fontWeight: "500",
     lineHeight: 26,
     letterSpacing: -1.4,
   },
   subtitleMedium14: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 14,
     fontWeight: "500",
     lineHeight: 18.2,
   },
   footerBold12: {
+    fontFamily: "NotoSans_700Bold",
     fontSize: 12,
     fontWeight: "700",
     lineHeight: 15.6,
   },
   bodyMedium11: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 11,
     fontWeight: "500",
     lineHeight: 14.3,
   },
   bodyRegular11: {
+    fontFamily: "NotoSans_400Regular",
     fontSize: 11,
     fontWeight: "400",
     lineHeight: 14.3,
   },
   labelMedium10: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 10,
     fontWeight: "500",
     lineHeight: 13,
   },
   bodyRegular10: {
+    fontFamily: "NotoSans_400Regular",
     fontSize: 10,
     fontWeight: "400",
     lineHeight: 13,
   },
   captionMedium9: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 9,
     fontWeight: "500",
     lineHeight: 11.7,
   },
   captionRegular9: {
+    fontFamily: "NotoSans_400Regular",
     fontSize: 9,
     fontWeight: "400",
     lineHeight: 11.7,
   },
   captionMedium8: {
+    fontFamily: "NotoSans_500Medium",
     fontSize: 8,
     fontWeight: "500",
     lineHeight: 10.4,
