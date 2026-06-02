09e75d12cb2f0072dd2df46000e407f6cedc32fb
minju3212 <smj3mjs@gmail.com>
2026-06-02 18:50:57 +0900

    ✨ feat: 탭바 아이콘 및 라벨 컴포넌트 적용

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { IconSymbol } from "@/components/ui/icon-symbol";
+import { TabBarIcon, TabBarLabel } from "@/app/_components/TabBarIcon";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
@@ -12,36 +12,44 @@ export default function TabLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: "홈",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="home" focused={focused} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel label="홈" focused={focused} />
           ),
         }}
       />
       <Tabs.Screen
         name="history"
         options={{
-          title: "청약 이력",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="history" focused={focused} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel label="청약 이력" focused={focused} />
           ),
         }}
       />
       <Tabs.Screen
         name="favorites"
         options={{
-          title: "관심 공모주",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="favorites" focused={focused} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel label="관심 공모주" focused={focused} />
           ),
         }}
       />
       <Tabs.Screen
         name="mypage"
         options={{
-          title: "마이페이지",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="mypage" focused={focused} />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <TabBarLabel label="마이페이지" focused={focused} />
           ),
         }}
       />
